6c738f1b044e8e622b700100f0a15217e41deea1
DougThwaites <doug@jumpfrog.co.nz>
2013-07-09 16:07:01 +1200

    Added credits

--- iOS-inc-greyscale.py
+++ iOS-inc-greyscale.py
@@ -1,3 +1,64 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------------
+# All credits for research that made this tool possible go to
+# the oringinal iOS artwork extractor tool from Dave Peck.
+# 
+# Twitter: @DangerDave
+# Website: davepeck.org
+# 
+# Oringinal tool: http://github.com/davepeck/iOS-artwork/
+# 
+# To support Dave and keep yourself safe on public wifi on your apple devices
+# go to www.getcloak.com.  Cloak is the best way to encrypt your network traffic
+# while on open public wireless hotspots
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# iOS-inc-greyscale
+# 
+# This tool was made to allow for greyscale images to be imported into the iOS
+# artwork file.
+# 
+# It has only been well tested with the Shared@2x.artwork file but should work with
+# different artwork files that are layed out the same.
+# 
+# Please use this tool at your own risk, although we tested it with multiple iOS devices
+# including iphone's 4 and 5 and Ipads it could still be improved beyond its current state and
+# is not assumed to be anywhere near perfect.
+# 
+# To run this tool:
+# 
+#   iOS-inc-greyscale.py "Path/To/artwork.file" "Path/To/New/Artwork/Images" 
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# About the author:
+# 
+# I made this script to help a theme designer @Jato_BZ and it is functional but
+# by no means a reflection of clean and abstracted code, it was made and may be
+# maintained in my spare time or by the community.
+# 
+# If you have any questions feel free to email me at digi.doc.doug@gmail.com, depending on
+# my free time I will get back to you as soon as i can.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Thanks to:
+# 
+# Dave Peck
+# @DangerDave
+# Initial Research
+# Made an awesome tool to base this off
+# Dev @ getcloak.com
+# 
+# Matt Smith
+# @Jato_BZ
+# Initial tester
+# Theme Designer
+# iOS advice
+#-------------------------------------------------------------------------------
+
 import os
 import sys
 import PIL
@@ -17,13 +78,13 @@ def write_bytes(format, value, file):
 	return 1
 
 def bail(message):
-	#Took this from Dave Packs tool too, thanks Dave
+	#Took this from Dave Peck's tool too, thanks Dave
     print "\n%s\n" % message
     sys.exit(-1)
 
 def main():	
 
-	# Vars
+	#Vars
 	working_dir = os.getcwd()
 	dave_pecked_artwork_path = sys.argv[1]
 	os_path_seperator = os.sep
